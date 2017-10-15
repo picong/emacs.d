ffdab40a3d56de551f4b9a55a5a0e492bfda86b6
皮聪 <picong@wb.u51.com>
2017-10-15 21:59:55 +0800

    modeline使用子龙山人的配置

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,14 +1,38 @@
 (require-package 'nyan-mode)
 
-(setq mode-line-format
-      (list
-       '(:eval (list (nyan-create)))
-       ))
-(nyan-mode t)
-;;;(nyan-start-animation) ;;;可以让猫抖动
-
 ;;keybindings
 (global-set-key (kbd "C-s") 'sanityinc/swiper-at-point)
+(global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point+)
+(global-set-key (kbd "M-<left>") 'backward-word)
+(global-set-key (kbd "M-<right>") 'forward-word)
 
+;;ui
+;; (require 'spaceline-config)
+;; (spaceline-spacemacs-theme)
+;; (setq spaceline-window-numbers-unicode t)
+;; (after-load 'spaceline-config
+;;   (spaceline-toggle-minor-modes-off))
+;; (after-load 'spaceline-config
+;;   (spaceline-toggle-window-number-on))
+;; (after-load 'spaceline-config
+;;   (window-numbering-mode))
+;; (after-load 'init-modeline
+;;   (window-numbering-mode))
+
+
+;; (setq mode-line-format
+;;       (list
+;;        '(:eval (list (nyan-create)))
+;;        ))
+;; (after-load 'init-modeline
+;;   (nyan-mode t))
+
+;; other pakages setup
+(use-package vue-mode)
+(hlinum-activate)
+
+;; org-mode
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
 (provide 'init-local)
